5a1b4c1abb463a6306d7f9bd46a0e173314d91c6
Barak2222 <barakcohen54@gmail.com>
2016-05-06 20:17:49 +0300

    refactored steps in JS

--- app/js/newjs.js
+++ app/js/newjs.js
@@ -1,4 +1,45 @@
 
+var step1 = {
+  init: function(){
+    
+  }
+}
+
+var step2 = {
+  distance: {
+    min: null,
+    max: null
+  }
+  location: {
+    log: null,
+    lat: null
+  },
+  init: function(log, lat){
+    this.location.log = log;
+    this.location.lat = lat;
+  }
+
+  getData: function(){
+    var temp = rangeSlider.getData();
+    this.distance.min = temp[0];
+    this.distance.max = temp[1];
+    return (this.distance.min > 0 && this.distance.max > 0);
+  }
+
+  launch: function(){
+    if(step2.getData()){
+      step3.init();
+    } else {
+      console.log("error in step2");
+    }
+  }
+}
+
+var step3 = {
+  init: function(){
+
+  }
+}
 
 
 var x = document.getElementById("error");
@@ -25,7 +66,6 @@ var x = document.getElementById("error");
 
 window.onload = function(){
   rangeSlider.init();
-  rangeSlider.getData();
 }
 
 var rangeSlider = {
@@ -35,7 +75,6 @@ var rangeSlider = {
   },
   getData: function(){
     var value = rangeSlider.s.slider('getValue')
-    console.log(value);
     return value;
   }
 }
