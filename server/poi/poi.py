4709667f9c718c43c6575215ab0895aa13f54adc
Muller <Arik.Muller@emc.com>
2016-05-06 16:42:56 +0300

    Added geoname_id to POI class and get radius from args

--- server/poi/poi.py
+++ server/poi/poi.py
@@ -1,5 +1,7 @@
 class POI:
-    def __init__(self, title=None, lon=None, lat=None, yapq_grade=None):
+    def __init__(self, geoname_id= None, title=None,
+                 lon=None, lat=None, yapq_grade=None):
+        self.geoname_id = geoname_id
         self.title = title
         self.lon = lon
         self.lat = lat
