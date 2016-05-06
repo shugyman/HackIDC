6070863ea60a0e01e2ae9f822c54a308eb53dfc0
Barak2222 <barakcohen54@gmail.com>
2016-05-06 21:41:21 +0300

    step2

--- app/js/newjs.js
+++ app/js/newjs.js
@@ -18,7 +18,6 @@ var app = {
   }
 }
 
-
 var step1 = {
   myLocation: {
     log: null,
@@ -27,7 +26,7 @@ var step1 = {
   otherLocation: {
     log: null,
     lat: null
-  }
+  },
   isLocationAvailable: false,
 
   init: function(){
@@ -41,7 +40,7 @@ var step1 = {
     //update log and lat
   },
   otherLocationClicked: function(){
-    
+
   }
 }
 
@@ -55,6 +54,7 @@ var step2 = {
     lat: null
   },
   init: function(log, lat){
+    console.log('step 2 inititialized');
     this.location.log = log;
     this.location.lat = lat;
   },
@@ -65,21 +65,40 @@ var step2 = {
     return (this.distance.min > 0 && this.distance.max > 0);
   },
   launch: function(){
-    if(step2.getData()){
+    if(step2.getData() && step2.location.log != null && step2.location.lat != null){
       step3.init();
     } else {
       console.log("error in step2");
-      console.log(step2);
+      console.log(step2.distance);
+      console.log(step2.location);
     }
   },
 }
 
 var step3 = {
+  data: null,
+  
+  tempData: [{"geoname_id": 6254976, "lat": 48.8582, "lon": 2.2945, "yapq_grade": 5, "title": "Eiffel Tower"}, {"geoname_id": 6269533, "lat": 48.8738, "lon": 2.295, "yapq_grade": 4.39, "title": "Arc de Triomphe"}, {"geoname_id": 6452892, "lat": 48.8867, "lon": 2.343, "yapq_grade": 4.36, "title": "Sacr\u00e9-C\u0153ur, Paris"}, {"geoname_id": 6269274, "lat": 48.853, "lon": 2.3498, "yapq_grade": 4, "title": "Notre Dame de Paris"}, {"geoname_id": 0, "lat": 48.8622, "lon": 2.3325, "yapq_grade": 3.52, "title": "Tuileries Palace"}],
   init: function(){
+    if(true){ // FOR DEMO PURPOSES
+      console.log(this.tempData);
+      return ;
+    }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+     step3.data = xhttp.responseText;
+     console.log(step3.data);
+    }
+  };
+  xhttp.open("GET", "demo_get2.asp?fname=Henry&lname=Ford", true);
+  xhttp.send();
+  },
+
+  createStep: function(){
 
   }
 };
-
 
 
 var x = document.getElementById("error");
