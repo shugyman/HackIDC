7672e1ac06156d6d381951576f0f495e84b1d226
Shugy <shugy@shugy.shugy>
2016-05-06 18:13:02 +0300

    Started routes calculator

--- server/poi/poi_calculator.py
+++ server/poi/poi_calculator.py
@@ -1,25 +1,31 @@
 from poi import POI
+from cache import POICache
+
 import requests
+
 
 def get_pois(latitude, longitude, rad):
     pois_lst = []
     yapq_url = "https://api.sandbox.amadeus.com/v1.2/points-of-interest/yapq-search-circle"
     payload = {
-    "apikey": "hZGFYuvJygHIZ0c61Bv5HWxjwqmyKzoI",
-    "latitude": latitude,
-    "longitude": longitude,
-    "radius": rad,
-    "category": "landmark",
-    "number_of_images": 0,
+        "apikey": "hZGFYuvJygHIZ0c61Bv5HWxjwqmyKzoI",
+        "latitude": latitude,
+        "longitude": longitude,
+        "radius": rad,
+        "category": "landmark",
+        "number_of_images": 0,
     }
     r = requests.get(yapq_url, params=payload)
     if r.status_code == 200:
         for poi in r.json().get('points_of_interest'):
-            pois_lst.append(POI(**parse_poi_dict(poi)))
+            poi_obj = POI(**parse_poi_dict(poi))
+            POICache().cache[poi_obj.geoname_id] = poi_obj
+            pois_lst.append(poi_obj)
     else:
         return None
 
     return pois_lst
+
 
 def parse_poi_dict(poi):
     geoname_id = poi.get('geoname_id', "N/A")
