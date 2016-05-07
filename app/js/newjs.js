1cc9273e3fdb3daccee3234b3d22e3054c817426
Asaf Fried <rutiearens@Ruties-MacBook-Air.local>
2016-05-06 20:25:37 -0700

    a

--- app/js/newjs.js
+++ app/js/newjs.js
@@ -1,5 +1,13 @@
 
+$(document).ready(function(){
+$(".btn-default").click(function(){
+   $("#sec01but02").toggleClass("btn-info");
+   $("#sec01but01").toggleClass("btn-info");
+ 
+});
 
+
+});
 
 var x = document.getElementById("error");
   function getLocation() {
@@ -13,7 +21,12 @@ var x = document.getElementById("error");
   function showPosition(position) {
     var lat = position.coords.latitude; 
     var lon = position.coords.longitude;
-    console.log(lat + " " + lon);
+    var latlon = 2.2 + "," + 35;
+
+    var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
+    +latlon+"&zoom=14&size=400x300&sensor=false";
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+
 }
   function getDistance() {
     $("#loc2").show();
